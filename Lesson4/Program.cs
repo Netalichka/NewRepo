ac827607bec126bdf31a23308e6b54fd63370780
Netalichka <netalichka2@gmail.com>
2022-04-10 20:54:31 +0300

    Добавьте файлы проекта.

---
+++ Lesson4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class MyArray { 
+
+  static void Main(string[] args)
+    {
+
+    int[] array = new int[20];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        { 
+        array[i] = rnd.Next(-10000, 10000);
+
+            Console.Write($"{array[i]}\t");
+
+        }
+ 
+
+
+    }
+}
